e40565c1b82939fc3441cce2ba9e76ba2433f3cd
Bill Loats <loatswil@gmail.com>
2020-02-01 08:48:47 -0600

    Simplfy

--- Get-News.ps1
+++ Get-News.ps1
@@ -15,82 +15,34 @@
   
 #>
 
-<# TLS Notes
 
-  TLS errors might persist if PowerShell uses TLS 1 (default)
-    This can be fixed with this command.
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    For details:
-    https://stackoverflow.com/questions/41618766/powershell-invoke-webrequest-fails-with-ssl-tls-secure-channel
-
-#>
-
-<# Feed Updates!!!
-
-Added 
-
-https://www.engadget.com/rss.xml
-https://www.politico.com/
-https://feeds.a.dj.com/rss/RSSWorldNews.xml
-https://feeds.a.dj.com/rss/RSSWSJD.xml
-https://theweek.com/rss.xml
-https://www.foreignaffairs.com/
-https://www.theguardian.com/world/rss
-http://rss.cnn.com/rss/edition_world.rss
-https://www.yahoo.com/news/world/rss
-http://www.aljazeera.com/xml/rss/all.xml
-https://www.buzzfeed.com/world.xml
-http://feeds.bbci.co.uk/news/world/rss.xml
-http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml
-http://feeds.bbci.co.uk/news/technology/rss.xml
-http://feeds.reuters.com/Reuters/domesticNews
-http://feeds.reuters.com/Reuters/worldNews
-http://feeds.reuters.com/reuters/technologyNews
-https://rssfeeds.usatoday.com/UsatodaycomWorld-TopStories
-https://rssfeeds.usatoday.com/UsatodaycomNation-TopStories
-https://rssfeeds.usatoday.com/usatoday-TechTopStories
-
-Can't find reliabel RSS feeds
-
-https://www.bloomberg.com/
-
-Not added due to custom formatting
-
-https://www.theatlantic.com/
-https://archive.nytimes.com/www.nytimes.com/services/xml/rss/index.html
-https://www.ap.org/en-us/
-
-Stale feeds
-
-http://feeds.washingtonpost.com/rss/rss_morning-mix
-http://feeds.arstechnica.com/arstechnica/apple 
-http://feeds.arstechnica.com/arstechnica/technology-lab
-http://feeds.arstechnica.com/arstechnica/security
-
-#>
+<#  Simplifying for now 
+    Will add back later for blacklisting
 
 [CmdletBinding()]
 
 Param(
         [Parameter(Mandatory=$false)]
-        [System.Management.Automation.ValidateNotNullOrEmptyAttribute()]
         [switch] $ShowOutput,
         [Parameter(Mandatory=$false)]
-        [System.Management.Automation.ValidateNotNullOrEmptyAttribute()]
         [switch] $Whitelist,
         [Parameter(Mandatory=$false)]
-        [System.Management.Automation.ValidateNotNullOrEmptyAttribute()]
         [string] $Blacklist
 )
 
-$allstories = @()
+#>
 
+$allstories = @()
+$Output = "~/Desktop/News.html"
+
+$EconUS = Invoke-WebRequest https://www.economist.com/united-states/rss.xml
+$EconWorld = Invoke-WebRequest https://www.economist.com/international/rss.xml
+$EconTech = Invoke-WebRequest https://www.economist.com/science-and-technology/rss.xml
 $Engadget = Invoke-WebRequest https://www.engadget.com/rss.xml
 $GuadrianWorld = Invoke-WebRequest https://www.theguardian.com/world/rss
 $CNNWorld = Invoke-WebRequest http://rss.cnn.com/rss/edition_world.rss
 $YahooWorld = Invoke-WebRequest https://www.yahoo.com/news/world/rss
 $AlJazeeraAll = Invoke-WebRequest http://www.aljazeera.com/xml/rss/all.xml
-$BuzzfeedWorld = Invoke-WebRequest https://www.buzzfeed.com/world.xml
 $BBCWorld = Invoke-WebRequest http://feeds.bbci.co.uk/news/world/rss.xml
 $BBCUS = Invoke-WebRequest http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml
 $BBCTech = Invoke-WebRequest http://feeds.bbci.co.uk/news/technology/rss.xml
@@ -143,9 +95,12 @@ function PullNews2($feed) {
     $stories
 }
 
+
+$allstories += PullNews2($EconUS)
+$allstories += PullNews2($EconWorld)
+$allstories += PullNews2($EconTech)
 $allstories += PullNews1($Engadget)
 $allstories += PullNews1($CNNWorld)
-$allstories += PullNews2($BuzzfeedWorld) 
 $allstories += PullNews2($GuadrianWorld)
 $allstories += PullNews2($YahooWorld)
 $allstories += PullNews1($AlJazeeraAll)
@@ -168,62 +123,31 @@ $allstories += PullNews2($POEnergy)
 
 $allstories = $allstories | Sort-Object -Property pubdate -Descending
 
-function WriteFile($Blacklist, $Whitelist) {
-        if ($Blacklist) {
-            Add-Content -Value (Write-Output "<b>News - Blacklisting: $BlackList</b>") -Path $Output
-            Add-Content -Value (Get-date) -Path $Output
-            Add-Content -Value ("<br>") -Path $Output
-            foreach($story in $allstories) {
-                If ($story.title -notmatch $BlackList){
-                    Add-Content -Value (Write-Output "<font size=""-3"">"$story.pubdate "</font>") -Path $Output       
-                    Add-Content -Value (Write-Output "&nbsp;") -Path $Output
-                    Add-Content -Value (Write-Output "<a href=""") -Path $Output
-                    Add-Content -Value ($story.link) -Path $Output
-                    Add-Content -Value (Write-Output """>") -Path $Output
-                    Add-Content -Value ($story.title) -Path $Output
-                    Add-Content -Value (Write-Output "</a>") -Path $Output
-                    Add-Content -Value (Write-Output "<font size=""-1"">"$story.feedtitle "</font>") -Path $Output
-                    Add-Content -Value (Write-Output "<br>") -Path $Output
-                    }
-        Else {
-            Add-Content -Value (Write-Output "<b>News</b>") -Path $Output
-            Add-Content -Value (Get-date) -Path $Output
-            Add-Content -Value ("<br>") -Path $Output
-            foreach($story in $allstories) {
-                Add-Content -Value (Write-Output "<font size=""-3"">"$story.pubdate "</font>") -Path $Output       
-                Add-Content -Value (Write-Output "&nbsp;") -Path $Output
-                Add-Content -Value (Write-Output "<a href=""") -Path $Output
-                Add-Content -Value ($story.link) -Path $Output
-                Add-Content -Value (Write-Output """>") -Path $Output
-                Add-Content -Value ($story.title) -Path $Output
-                Add-Content -Value (Write-Output "</a>") -Path $Output
-                Add-Content -Value (Write-Output "<font size=""-1"">"$story.feedtitle "</font>") -Path $Output
-                Add-Content -Value (Write-Output "<br>") -Path $Output
-                }
-            }  
-        }
-    }
-}
-function WriteHost {
-    Write-Host "News"(Get-Date)
+function WriteFile() {
+    Add-Content -Value (Write-Output "<b>News</b>") -Path $Output
+    Add-Content -Value (Get-date) -Path $Output
+    Add-Content -Value ("<br>") -Path $Output
     foreach($story in $allstories) {
-        $story.pubdate
-        $story.title
+        Add-Content -Value (Write-Output "<font size=""-1"">"$story.pubdate "</font>") -Path $Output       
+        Add-Content -Value (Write-Output "&nbsp;") -Path $Output
+        Add-Content -Value (Write-Output "<a href=""") -Path $Output
+        Add-Content -Value ($story.link) -Path $Output
+        Add-Content -Value (Write-Output """>") -Path $Output
+        Add-Content -Value ($story.title) -Path $Output
+        Add-Content -Value (Write-Output "</a>") -Path $Output
+        #Add-Content -Value (Write-Output "<font size=""+1"">"$story.feedtitle "</font>") -Path $Output
+        Add-Content -Value (Write-Output "<br>") -Path $Output
         }
-    }
+        Add-Content -Value (Write-Output "<br><a href="readme.txt">readme.txt</a>") -Path $Output
+}  
 
-if($ShowOutput) {
-    WriteHost
-    Exit
-}
-else {
-    $Output = "~/Desktop/News.html"
-    if (Test-Path $Output) {
-        Clear-Content $Output
-        }
-        else {
-            New-Item -Path $Output -ItemType File -Confirm
-        }
-    WriteFile($BlackList)
-    Invoke-Item $Output
-}
+
+if (Test-Path $Output) {
+    Clear-Content $Output
+    }
+    else {
+        New-Item -Path $Output -ItemType File -Confirm
+    }
+WriteFile
+Invoke-Item $Output
+Copy-Item -Path .\README.md -Destination .\readme.txt
