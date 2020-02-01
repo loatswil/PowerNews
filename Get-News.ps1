4e978263ce92fa5be58004a3add00d90acf6ccaa
Bill Loats <loatswil@gmail.com>
2020-02-01 16:24:00 -0600

    Importing CSV for sources now

--- Get-News.ps1
+++ Get-News.ps1
@@ -15,54 +15,11 @@
   
 #>
 
-
-<#  Simplifying for now 
-    Will add back later for blacklisting
-
-[CmdletBinding()]
-
-Param(
-        [Parameter(Mandatory=$false)]
-        [switch] $ShowOutput,
-        [Parameter(Mandatory=$false)]
-        [switch] $Whitelist,
-        [Parameter(Mandatory=$false)]
-        [string] $Blacklist
-)
-
-#>
-
+$allfeeds = @()
 $allstories = @()
 $Output = "~/Desktop/News.html"
 
-
-$NPRUS = Invoke-WebRequest https://www.npr.org/rss/rss.php?id=1003
-$NPRWorld = Invoke-WebRequest https://www.npr.org/rss/rss.php?id=1004
-$EconUS = Invoke-WebRequest https://www.economist.com/united-states/rss.xml
-$EconWorld = Invoke-WebRequest https://www.economist.com/international/rss.xml
-$EconTech = Invoke-WebRequest https://www.economist.com/science-and-technology/rss.xml
-$Engadget = Invoke-WebRequest https://www.engadget.com/rss.xml
-$GuadrianWorld = Invoke-WebRequest https://www.theguardian.com/world/rss
-$CNNWorld = Invoke-WebRequest http://rss.cnn.com/rss/edition_world.rss
-$YahooWorld = Invoke-WebRequest https://www.yahoo.com/news/world/rss
-$AlJazeeraAll = Invoke-WebRequest http://www.aljazeera.com/xml/rss/all.xml
-$BBCWorld = Invoke-WebRequest http://feeds.bbci.co.uk/news/world/rss.xml
-$BBCUS = Invoke-WebRequest http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml
-$BBCTech = Invoke-WebRequest http://feeds.bbci.co.uk/news/technology/rss.xml
-$ReutersUS = Invoke-WebRequest http://feeds.reuters.com/Reuters/domesticNews
-$ReutersWorld = Invoke-WebRequest http://feeds.reuters.com/Reuters/worldNews
-$ReutersTech = Invoke-WebRequest http://feeds.reuters.com/reuters/technologyNews
-$WSJWorld = Invoke-WebRequest https://feeds.a.dj.com/rss/RSSWorldNews.xml
-$WSJTech = Invoke-WebRequest https://feeds.a.dj.com/rss/RSSWSJD.xml
-$FA = Invoke-WebRequest https://www.foreignaffairs.com/rss.xml
-$USAWorld = Invoke-WebRequest https://rssfeeds.usatoday.com/UsatodaycomWorld-TopStories
-$USANational = Invoke-WebRequest https://rssfeeds.usatoday.com/UsatodaycomNation-TopStories
-$USATech = Invoke-WebRequest https://rssfeeds.usatoday.com/usatoday-TechTopStories
-$THEWEEK = Invoke-WebRequest https://theweek.com/rss.xml
-$POCongress = Invoke-WebRequest http://www.politico.com/rss/congress.xml
-$PODefense = Invoke-WebRequest http://www.politico.com/rss/defense.xml
-$POEnergy = Invoke-WebRequest http://www.politico.com/rss/energy.xml
-
+$allfeeds = Import-Csv -Path .\news-sources.csv
 function PullNews1($feed) {
     [xml]$feedxml = $feed.Content
     $stories = @()
@@ -98,36 +55,18 @@ function PullNews2($feed) {
     $stories
 }
 
+ForEach ($feed in $allfeeds) {
+    $rawfeed = Invoke-WebRequest $feed.feed
+    if ($feed.type -like "1") {
+        $allstories += PullNews1($rawfeed)
+    }
+    else {
+        $allstories += PullNews2($rawfeed)
+    }
 
-$allstories += PullNews2($NPRWorld)
-$allstories += PullNews2($NPRUS)
-$allstories += PullNews2($EconUS)
-$allstories += PullNews2($EconWorld)
-$allstories += PullNews2($EconTech)
-$allstories += PullNews1($Engadget)
-$allstories += PullNews1($CNNWorld)
-$allstories += PullNews2($GuadrianWorld)
-$allstories += PullNews2($YahooWorld)
-$allstories += PullNews1($AlJazeeraAll)
-$allstories += PullNews1($BBCWorld)
-$allstories += PullNews1($BBCUS)
-$allstories += PullNews1($BBCTech)
-$allstories += PullNews2($ReutersUS)
-$allstories += PullNews2($ReutersTech)
-$allstories += PullNews2($ReutersWorld)
-$allstories += PullNews1($THEWEEK)
-$allstories += PullNews2($WSJWorld)
-$allstories += PullNews2($WSJTech)
-$allstories += PullNews2($USAWorld)
-$allstories += PullNews2($USANational)
-$allstories += PullNews2($USATech)
-$allstories += PullNews2($FA)
-$allstories += PullNews2($POCongress)
-$allstories += PullNews2($PODefense)
-$allstories += PullNews2($POEnergy)
+}
 
 $allstories = $allstories | Sort-Object -Property pubdate -Descending
-
 function WriteFile() {
     Add-Content -Value (Write-Output "<b>News</b>") -Path $Output
     Add-Content -Value (Get-date) -Path $Output
@@ -145,7 +84,6 @@ function WriteFile() {
         }
         Add-Content -Value (Write-Output "<br><a href="readme.txt">readme.txt</a>") -Path $Output
 }  
-
 
 if (Test-Path $Output) {
     Clear-Content $Output
